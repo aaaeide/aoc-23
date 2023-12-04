6e9e66f45a0bae97cc4f2dae8433ef9e81f8ad02
aaaeide <andreasaaeide@gmail.com>
2023-12-04 22:25:23 +0100

    Add day 03

--- main.go
+++ main.go
@@ -5,30 +5,36 @@ import (
 
 	"github.com/aaaeide/aoc-23/day01"
 	"github.com/aaaeide/aoc-23/day02"
+	"github.com/aaaeide/aoc-23/day03"
 )
+
+func runPart(i string, part func(*os.File) string) string {
+	filename := "inputs/day0" + i + ".txt"
+	file, err := os.Open(filename)
+	
+	if err != nil {
+		return "\nCOULD NOT OPEN FILE " + filename
+	}
+	
+	defer file.Close()
+
+	return part(file)
+}
 
 func runDay(
 	i string, 
 	part1 func(*os.File) string, 
 	part2 func(*os.File) string,
 ) {
-	filename := "inputs/day0" + i + ".txt"
-	file, err := os.Open(filename)
-	
-	if err != nil {
-		println("\nCOULD NOT OPEN FILE", filename)
-		return
-	}
-	
-	defer file.Close()
 
 	println("\n==================\nDay 0" + i + "\n------------------")
-	println("Part 1:", part1(file))
-	println("Part 2:", part2(file))
+	println("Part 1:", runPart(i, part1))
+	println("Part 2:", runPart(i, part2))
 	println("==================")
 } 
 
 func main() {
 	runDay("1", day01.Part1, day01.Part2)
 	runDay("2", day02.Part1, day02.Part2)
+	runDay("3", day03.Part1, day03.Part2)
 }
