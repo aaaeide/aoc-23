0f4f0cc88ae6e80f5a6f16610a203504ff7a25f8
aaaeide <andreasaaeide@gmail.com>
2023-12-10 14:28:15 +0100

    Update default test file name

--- daytemplate/main_test.go
+++ daytemplate/main_test.go
@@ -7,9 +7,9 @@ package daytemplate
 // )
 
 // func TestPart1(t *testing.T) {
-// 	utils.Tester(t, "test1.txt", Part1, "_")()
+// 	utils.Tester(t, "test.txt", Part1, "_")()
 // }
 
 // func TestPart2(t *testing.T) {
-// 	utils.Tester(t, "test2.txt", Part2, "_")()
+// 	utils.Tester(t, "test.txt", Part2, "_")()
 // }
